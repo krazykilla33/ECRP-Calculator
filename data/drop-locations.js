376e2b9dac627c4991071e0e6c795b503d083abb
krazykilla33 <jonathan3337@gmail.com>
2026-07-15 00:30:47 -0400

    Create drop-locations.js

---
+++ data/drop-locations.js
@@ -0,0 +1,869 @@
+window.DROP_LOCATIONS = [
+  {
+    "id": "rons-wind-farm",
+    "name": "Rons Wind Farm",
+    "helper": "Inside the gated area on the west side of Wind Farm",
+	"image": "assets/drop-hints/rons-wind-farm.jpg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1290,
+        "y": 986
+      },
+      "atlas": {
+        "x": 1290,
+        "y": 987
+      }
+    }
+  },
+  {
+    "id": "pipeline-inn",
+    "name": "Pipeline Inn",
+    "helper": "",
+	"image": "assets/drop-hints/pipeline-inn.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 576,
+        "y": 598
+      },
+      "atlas": {
+        "x": 575,
+        "y": 601
+      }
+    }
+  },
+  {
+    "id": "fort-zancudo",
+    "name": "Fort Zancudo",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 421,
+        "y": 1204
+      },
+      "atlas": {
+        "x": 421,
+        "y": 1205
+      }
+    }
+  },
+  {
+    "id": "north-chumash",
+    "name": "North Chumash",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 542,
+        "y": 1363
+      },
+      "atlas": {
+        "x": 543,
+        "y": 1365
+      }
+    }
+  },
+  {
+    "id": "raton-canyon-waterfall",
+    "name": "Raton Canyon Waterfall",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 642,
+        "y": 1427
+      },
+      "atlas": {
+        "x": 642,
+        "y": 1428
+      }
+    }
+  },
+  {
+    "id": "paleto-cove",
+    "name": "Paleto Cove",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 679,
+        "y": 1516
+      },
+      "atlas": {
+        "x": 679,
+        "y": 1518
+      }
+    }
+  },
+  {
+    "id": "paleto-forest-3",
+    "name": "Paleto Forest 3",
+    "helper": "Under the bridge",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 819,
+        "y": 1563
+      },
+      "atlas": {
+        "x": 818,
+        "y": 1565
+      }
+    }
+  },
+  {
+    "id": "paleto-forest-2",
+    "name": "Paleto Forest 2",
+    "helper": "Under the bridge",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 820,
+        "y": 1518
+      },
+      "atlas": {
+        "x": 820,
+        "y": 1519
+      }
+    }
+  },
+  {
+    "id": "paleto-forest-1",
+    "name": "Paleto Forest 1",
+    "helper": "Under the train tracks",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 864,
+        "y": 1570
+      },
+      "atlas": {
+        "x": 864,
+        "y": 1571
+      }
+    }
+  },
+  {
+    "id": "paleto-bay",
+    "name": "Paleto Bay",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 816,
+        "y": 1663
+      },
+      "atlas": {
+        "x": 816,
+        "y": 1664
+      }
+    }
+  },
+  {
+    "id": "procopio-beach",
+    "name": "Procopio Beach",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1172,
+        "y": 1751
+      },
+      "atlas": {
+        "x": 1173,
+        "y": 1752
+      }
+    }
+  },
+  {
+    "id": "mount-gordo-lake",
+    "name": "Mount Gordo Lake",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1367,
+        "y": 1685
+      },
+      "atlas": {
+        "x": 1367,
+        "y": 1685
+      }
+    }
+  },
+  {
+    "id": "mount-gordo",
+    "name": "Mount Gordo",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1490,
+        "y": 1574
+      },
+      "atlas": {
+        "x": 1491,
+        "y": 1576
+      }
+    }
+  },
+  {
+    "id": "el-gordo-lighthouse",
+    "name": "El Gordo Lighthouse",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1480,
+        "y": 1516
+      },
+      "atlas": {
+        "x": 1480,
+        "y": 1517
+      }
+    }
+  },
+  {
+    "id": "san-chianski-mountain-range",
+    "name": "San Chianski Mountain Range",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1569,
+        "y": 1401
+      },
+      "atlas": {
+        "x": 1570,
+        "y": 1402
+      }
+    }
+  },
+  {
+    "id": "humane-labs",
+    "name": "Humane Labs",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1528,
+        "y": 1269
+      },
+      "atlas": {
+        "x": 1528,
+        "y": 1270
+      }
+    }
+  },
+  {
+    "id": "grapeseed-hangar",
+    "name": "Grapeseed Hangar",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1290,
+        "y": 1453
+      },
+      "atlas": {
+        "x": 1290,
+        "y": 1455
+      }
+    }
+  },
+  {
+    "id": "grapeseed-pier",
+    "name": "Grapeseed Pier",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1334,
+        "y": 1385
+      },
+      "atlas": {
+        "x": 1333,
+        "y": 1384
+      }
+    }
+  },
+  {
+    "id": "sandy-shores-jetty",
+    "name": "Sandy Shores Jetty",
+    "helper": "At the end of the point.",
+	"image": "assets/drop-hints/sandy-shores-jetty.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1196,
+        "y": 1310
+      },
+      "atlas": {
+        "x": 1197,
+        "y": 1311
+      }
+    }
+  },
+  {
+    "id": "stab-city-island",
+    "name": "Stab City Island",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 981,
+        "y": 1259
+      },
+      "atlas": {
+        "x": 980,
+        "y": 1260
+      }
+    }
+  },
+  {
+    "id": "route-68",
+    "name": "Route 68",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1180,
+        "y": 1129
+      },
+      "atlas": {
+        "x": 1181,
+        "y": 1130
+      }
+    }
+  },
+  {
+    "id": "zancudo-bridge",
+    "name": "Zancudo Bridge",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 869,
+        "y": 1156
+      },
+      "atlas": {
+        "x": 868,
+        "y": 1157
+      }
+    }
+  },
+  {
+    "id": "lago-zancudo-1",
+    "name": "Lago Zancudo 1",
+    "helper": "AKA Zancudo Lake",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 595,
+        "y": 1098
+      },
+      "atlas": {
+        "x": 596,
+        "y": 1099
+      }
+    }
+  },
+  {
+    "id": "lago-zancudo-2",
+    "name": "Lago Zancudo 2",
+    "helper": "AKA Zancudo Lake",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 505,
+        "y": 1105
+      },
+      "atlas": {
+        "x": 505,
+        "y": 1105
+      }
+    }
+  },
+  {
+    "id": "lago-zancudo-3",
+    "name": "Lago Zancudo 3",
+    "helper": "AKA Zancudo Lake",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 480,
+        "y": 1082
+      },
+      "atlas": {
+        "x": 480,
+        "y": 1083
+      }
+    }
+  },
+  {
+    "id": "chumash",
+    "name": "Chumash",
+    "helper": "Under the road",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 428,
+        "y": 868
+      },
+      "atlas": {
+        "x": 428,
+        "y": 869
+      }
+    }
+  },
+  {
+    "id": "vinewood-hills-water-drum",
+    "name": "Vinewood Hills Water Drum",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 908,
+        "y": 922
+      },
+      "atlas": {
+        "x": 907,
+        "y": 922
+      }
+    }
+  },
+  {
+    "id": "palmer-taylor-power-station",
+    "name": "Palmer Taylor Power Station",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1393,
+        "y": 886
+      },
+      "atlas": {
+        "x": 1393,
+        "y": 888
+      }
+    }
+  },
+  {
+    "id": "palomino-highlands",
+    "name": "Palomino Highlands",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1397,
+        "y": 549
+      },
+      "atlas": {
+        "x": 1398,
+        "y": 549
+      }
+    }
+  },
+  {
+    "id": "puerto-del-sol",
+    "name": "Puerto Del Sol",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 779,
+        "y": 423
+      },
+      "atlas": {
+        "x": 779,
+        "y": 423
+      }
+    }
+  },
+  {
+    "id": "ls-airport-west-hangar",
+    "name": "LS Airport West Hangar",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 667,
+        "y": 149
+      },
+      "atlas": {
+        "x": 666,
+        "y": 148
+      }
+    }
+  },
+  {
+    "id": "ls-airport-east-yard",
+    "name": "LS Airport East Yard",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 773,
+        "y": 89
+      },
+      "atlas": {
+        "x": 774,
+        "y": 86
+      }
+    }
+  },
+  {
+    "id": "ls-airport-trailer-yard",
+    "name": "LS Airport Trailer Yard",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 808,
+        "y": 229
+      },
+      "atlas": {
+        "x": 808,
+        "y": 228
+      }
+    }
+  },
+  {
+    "id": "elysian-island-west",
+    "name": "Elysian Island West",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 864,
+        "y": 265
+      },
+      "atlas": {
+        "x": 864,
+        "y": 266
+      }
+    }
+  },
+  {
+    "id": "elysian-island-south",
+    "name": "Elysian Island South",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 977,
+        "y": 119
+      },
+      "atlas": {
+        "x": 978,
+        "y": 121
+      }
+    }
+  },
+  {
+    "id": "terminal",
+    "name": "Terminal",
+    "helper": "",
+	"image": "",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1151,
+        "y": 139
+      },
+      "atlas": {
+        "x": 1150,
+        "y": 140
+      }
+    }
+  },
+  {
+    "id": "dutch-london-silos",
+    "name": "Dutch London Silos",
+    "helper": "Inside of the drive through labeled “2”",
+	"image": "assets/drop-hints/dutch-london-silos.jpg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1018,
+        "y": 308
+      },
+      "atlas": {
+        "x": 1019,
+        "y": 309
+      }
+    }
+  },
+  {
+    "id": "vineyard",
+    "name": "Vineyard",
+    "helper": "Center of the Vineyard",
+	"image": "assets/drop-hints/vineyard.jpg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 639,
+        "y": 1026
+      },
+      "atlas": {
+        "x": 639,
+        "y": 1027
+      }
+    }
+  },
+  {
+    "id": "sandy-motel",
+    "name": "Sandy Motel",
+    "helper": "In the drained swimming pool",
+	"image": "assets/drop-hints/sandy-motel.jpg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1197,
+        "y": 1255
+      },
+      "atlas": {
+        "x": 1197,
+        "y": 1256
+      }
+    }
+  },
+  {
+    "id": "alien-park",
+    "name": "Alien Park",
+    "helper": "",
+	"image": "assets/drop-hints/alien-park.jpg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1355,
+        "y": 1292
+      },
+      "atlas": {
+        "x": 1355,
+        "y": 1293
+      }
+    }
+  },
+  {
+    "id": "alta-construction",
+    "name": "Alta Construction",
+    "helper": "Take the red elevator upstairs then head to the middle of the building",
+	"image": "assets/drop-hints/alta-construction.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 913,
+        "y": 508
+      },
+      "atlas": {
+        "x": 912,
+        "y": 510
+      }
+    }
+  },
+  {
+    "id": "los-santos-river-2",
+    "name": "Los Santos River 2",
+    "helper": "",
+	"image": "assets/drop-hints/los-santos-river-2.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1044,
+        "y": 579
+      },
+      "atlas": {
+        "x": 1043,
+        "y": 581
+      }
+    }
+  },
+  {
+    "id": "los-santos-river-3",
+    "name": "Los Santos River 3",
+    "helper": "Under the bridge in Los Santos River. It is possible there is a second location for this one.",
+	"image": "assets/drop-hints/los-santos-river-3.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1072,
+        "y": 413
+      },
+      "atlas": {
+        "x": 1072,
+        "y": 414
+      }
+    }
+  },
+  {
+    "id": "cayo-drum",
+    "name": "Cayo Drum",
+    "helper": "On Cayo Perico near the red water tower",
+	"image": "assets/drop-hints/cayo-drum.jpg",
+    "region": "cayo",
+    "map": {
+      "satellite": {
+        "x": 1105,
+        "y": 791
+      },
+      "atlas": {
+        "x": 1094,
+        "y": 805
+      }
+    }
+  },
+  {
+    "id": "elysian-freeway-bridge",
+    "name": "Elysian Freeway Bridge",
+    "helper": "Under the red bridge on the garbage side",
+	"image": "assets/drop-hints/elysian-freeway-bridge.jpg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1101,
+        "y": 242
+      },
+      "atlas": {
+        "x": 1101,
+        "y": 242
+      }
+    }
+  },
+  {
+    "id": "del-perro-pier",
+    "name": "Del Perro Pier",
+    "helper": "Under the pier on the Vespucci Beach side",
+	"image": "assets/drop-hints/del-perro-pier.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 670,
+        "y": 481
+      },
+      "atlas": {
+        "x": 670,
+        "y": 482
+      }
+    }
+  },
+  {
+    "id": "mirror-park-power-station",
+    "name": "Mirror Park Power Station",
+    "helper": "",
+	"image": "assets/drop-hints/mirror-park-power-station.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1074,
+        "y": 586
+      },
+      "atlas": {
+        "x": 1075,
+        "y": 589
+      }
+    }
+  },
+  {
+    "id": "redwood-lights-track",
+    "name": "Redwood Lights Track",
+    "helper": "Northwest side of the track",
+	"image": "assets/drop-hints/redwood-lights-track.jpg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1092,
+        "y": 1068
+      },
+      "atlas": {
+        "x": 1092,
+        "y": 1070
+      }
+    }
+  },
+  {
+    "id": "galilee-pier",
+    "name": "Galilee Pier",
+    "helper": "Right lake, end of docks",
+	"image": "assets/drop-hints/galilee-pier.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1153,
+        "y": 1363
+      },
+      "atlas": {
+        "x": 1154,
+        "y": 1365
+      }
+    }
+  },
+  {
+    "id": "utopia-gardens",
+    "name": "Utopia Gardens",
+    "helper": "Broken-down houses in the back-right of the cul-de-sac",
+	"image": "assets/drop-hints/utopia-gardens.jpeg",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 1167,
+        "y": 539
+      },
+      "atlas": {
+        "x": 1167,
+        "y": 540
+      }
+    }
+  },
+  {
+    "id": "vespucci-beach",
+    "name": "Vespucci Beach",
+    "helper": "In front of the rocks facing north",
+	"image": "assets/drop-hints/vespucci-beach.png",
+    "region": "san-andreas",
+    "map": {
+      "satellite": {
+        "x": 694,
+        "y": 423
+      },
+      "atlas": {
+        "x": 694,
+        "y": 424
+      }
+    }
+  }
+];
